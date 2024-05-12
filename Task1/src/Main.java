c355f62798c8fb544beb191b5c37d58b9b0ce333
Anastasia Valova <morugina.anast@mail.ru>
2024-05-12 19:00:21 +0300

    First Commit

---
+++ Task1/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите первое целое число a:");
+        int a = scanner.nextInt();
+
+        System.out.println("Введите второе целое число b:");
+        int b = scanner.nextInt();
+
+        if (a > b) {
+            System.out.println("a > b");
+        } else if (a < b) {
+            System.out.println("a < b");
+        } else {
+            System.out.println("a = b");
+        }
+
+        System.out.println("Сложение: " + (a + b));
+        System.out.println("Вычитание: " + (a - b));
+
+        if (b != 0) {
+            System.out.println("Деление: " + (a / b));
+        } else {
+            System.out.println("Деление на ноль невозможно");
+        }
+
+        System.out.println("Умножение: " + (a * b));
+    }
+}
